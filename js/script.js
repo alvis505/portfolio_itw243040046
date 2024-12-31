4567f3af3160d018587d5e754d369238693cba06
alvis505 <alfisberkuliah@gmail.com>
2024-12-31 10:54:16 +0700

    animation

--- js/script.js
+++ js/script.js
@@ -6,12 +6,12 @@ menuToggle.addEventListener("click", function () {
   nav.classList.toggle("slide");
 });
 
-// var typed = new Typed(".input", {
-//   strings: ["Frontend Developer", "UX Designer", "Web Developer"],
-//   typedSpeed: 70,
-//   backSpeed: 55,
-//   loop: true,
-// });
+var typed = new Typed(".input", {
+  strings: ["Frontend Developer", "UX Designer", "Web Developer"],
+  typedSpeed: 70,
+  backSpeed: 55,
+  loop: true,
+});
 
 // // bagian animasi
 // sr.reveal(".logo-text", { delay: 200, origin: "top" });
