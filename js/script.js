76bc424337eb00a2d1117d4c48f564b998d93984
alvis505 <alfisberkuliah@gmail.com>
2024-12-31 09:13:12 +0700

    rombak sedikit

--- js/script.js
+++ js/script.js
@@ -1,22 +1,20 @@
 /** creating button click show hide navbar **/
-var togglebtn = document.querySelector(".togglebtn");
-var nav = document.querySelector(".navlinks");
-var links = document.querySelector(".navlinks li");
+const menuToggle = document.querySelector(".menu-toggler input");
+const nav = document.querySelector("nav ul");
 
-togglebtn.addEventListener("click", function () {
-  this.classList.toggle("click");
-  nav.classList.toggle("open");
+menuToggle.addEventListener("click", function () {
+  nav.classList.toggle("slide");
 });
 
-var typed = new Typed(".input", {
-  strings: ["Frontend Developer", "UX Designer", "Web Developer"],
-  typedSpeed: 70,
-  backSpeed: 55,
-  loop: true,
-});
+// var typed = new Typed(".input", {
+//   strings: ["Frontend Developer", "UX Designer", "Web Developer"],
+//   typedSpeed: 70,
+//   backSpeed: 55,
+//   loop: true,
+// });
 
-// bagian animasi
-sr.reveal(".logo-text", { delay: 200, origin: "top" });
-sr.reveal(".logo", { delay: 450, origin: "right" });
-sr.reveal(".icons", { delay: 450, origin: "left" });
-sr.reveal(".scroll-down", { delay: 450, origin: "right" });
+// // bagian animasi
+// sr.reveal(".logo-text", { delay: 200, origin: "top" });
+// sr.reveal(".logo", { delay: 450, origin: "right" });
+// sr.reveal(".icons", { delay: 450, origin: "left" });
+// sr.reveal(".scroll-down", { delay: 450, origin: "right" });
